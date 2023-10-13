b8c60f9a06e863746bac0f13d831b91410cd3b93
ravindrapaswan2762 <paswanravindra209@gmail.com>
2023-10-13 16:39:46 +0530

    saving post to database

--- controllers/post_controllers.js
+++ controllers/post_controllers.js
@@ -1,3 +1,24 @@
-module.exports.post = function(req, res){
-    return res.end('<h1>Here is your all posts!</h1>');
+const Post = require('../models/post');
+
+module.exports.post = async function(req, res){
+    
+    try{
+        const newPost = await Post.create({
+            content: req.body.content,
+            user: req.user._id
+        })
+
+        const allPost = await Post.find({user: req.user._id});
+        console.log('allPost: ', allPost);
+
+        return res.render('../views/post', {
+            title: 'Post Page',
+            allPost: allPost
+        });
+    }catch(err){
+        if(err){
+            console.log('Error in creating a post.')
+        }
+    }
+
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const passport = require('passport');
 //--------------------------------
 
 const userController = require('../controllers/users_controller');//accessing user controlling
+const postController = require('../controllers/post_controllers');
 
 // if 'passport.checkAuthentication' is true then only the 'controllers.profile' called and render profile page, otherwise not.
 router.get('/profile', passport.checkAuthentication, userController.profile);
@@ -12,9 +13,9 @@ router.get('/sign-up', userController.signUp);
 router.get('/sign-in', userController.signIn);
 router.get('/sign-out', userController.signOut);
 router.post('/create', userController.create);
-router.get('/posts', require('../controllers/post_controllers').post)
+router.post('/posts', postController.post);
 
-// user is signing-in using 'passport.authenticate' where 'local' authentication strategy is used
+// user is signing-in using 'passport.authenticate' where 'local' authentication strategy is used.
 //                             inbuild function in pasport library 
 router.post('/create-session', passport.authenticate(
     'local', 
